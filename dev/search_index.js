0284d3d188d74c3955afa766fc8c9a2d2ad7223d
Documenter.jl <documenter@juliadocs.github.io>
2024-11-08 15:34:25 +0000

    build based on 3ce14cb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MovingWindowArrays","category":"page"},{"location":"#MovingWindowArrays","page":"Home","title":"MovingWindowArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MovingWindowArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MovingWindowArrays]","category":"page"}]
+}
