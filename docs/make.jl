4c95d3d96aec44ccde4cf3fd93163a07a0fb047d
Simeon David Schaub <simeon@schaub.rocks>
2024-11-08 23:14:42 +0100

    formatting using Runic

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,23 @@
 using RollingWindowArrays
 using Documenter
 
-DocMeta.setdocmeta!(RollingWindowArrays, :DocTestSetup, :(using RollingWindowArrays); recursive=true)
+DocMeta.setdocmeta!(RollingWindowArrays, :DocTestSetup, :(using RollingWindowArrays); recursive = true)
 
 makedocs(;
-    modules=[RollingWindowArrays],
-    authors="Simeon David Schaub <simeon@schaub.rocks> and contributors",
-    sitename="RollingWindowArrays.jl",
-    format=Documenter.HTML(;
-        canonical="https://simeonschaub.github.io/RollingWindowArrays.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [RollingWindowArrays],
+    authors = "Simeon David Schaub <simeon@schaub.rocks> and contributors",
+    sitename = "RollingWindowArrays.jl",
+    format = Documenter.HTML(;
+        canonical = "https://simeonschaub.github.io/RollingWindowArrays.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/simeonschaub/RollingWindowArrays.jl",
-    devbranch="main",
+    repo = "github.com/simeonschaub/RollingWindowArrays.jl",
+    devbranch = "main",
 )
