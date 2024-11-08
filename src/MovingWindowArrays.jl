f909654afc076ab50009502dfc000a9c8a5e1b4c
Simeon David Schaub <simeon@schaub.rocks>
2024-11-08 17:55:03 +0100

    initial implementation

--- src/MovingWindowArrays.jl
+++ src/MovingWindowArrays.jl
@@ -1,5 +1,35 @@
 module MovingWindowArrays
 
-# Write your package code here.
+using OffsetArrays
+
+export rolling
+
+struct MovingWindowVector{T, A <: AbstractVector} <: AbstractVector{T}
+    parent::A
+    before::Int
+    after::Int
+end
+
+function MovingWindowVector(parent::A, before::Int, after::Int) where {A <: AbstractVector}
+    T = Core.Compiler.return_type(view, Tuple{A, UnitRange{Int}})
+    return MovingWindowVector{T, A}(parent, before, after)
+end
+function Base.size((; parent, before, after)::MovingWindowVector)
+    return (size(parent, 1) - before - after,)
+end
+function Base.axes((; parent, before, after)::MovingWindowVector)
+    return (axes(parent, 1)[begin + before:end - after],)
+end
+Base.@propagate_inbounds function Base.getindex((; parent, before, after)::MovingWindowVector, i::Int)
+    return view(parent, i - before:i + after)
+end
+
+function rolling(x::AbstractVector, before::Int, after::Int; dims = nothing)
+    return MovingWindowVector(x, before, after)
+end
+
+function rolling(x::AbstractArray, before::Int, after::Int; dims)
+    return MovingWindowVector(eachslice(x; dims), before, after)
+end
 
 end
